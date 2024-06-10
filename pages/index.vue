ea510bc7904bf828781d9aac8cacf2f5c86639e1
dominhloc <dominhloc2510@gmail.com>
2024-06-10 18:26:56 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,34 @@
 <template>
-  <div class="flex justify-end bg-slate-900 p-1 space-x-3">
-    <div class="flex-1">
+  <div class="flex justify-end bg-slate-900 p-1">
+    <div class="flex-1 rounded-md">
       <div
         class="flex hover:bg-slate-600 font-serif hover:scale-110 duration-300 w-fit ml-10 rounded-md p-1 text-xl"
       >
         <Avatar
           src="https://vntime.org/wp-content/uploads/2020/04/100-%E1%BA%A2nh-n%E1%BB%81n-m%C3%A1y-t%C3%ADnh-%C4%91%E1%BA%B9p-nh%E1%BA%A5t-c%C3%B3-link-t%E1%BA%A3i-v%E1%BB%81-tr%E1%BB%B1c-ti%E1%BA%BFp.jpeg"
         />
-        <button class="flex justify-center items-center p-2 ml-2 text-white">
+        <button class="flex justify-center items-center p-2 text-white">
           IT Blogggg
         </button>
       </div>
     </div>
-
     <button
-      class="p-2 rounded-md text-white hover:bg-slate-600 hover:scale-110 duration-300"
+      class="p-2 ml-4 rounded-md text-white hover:bg-slate-800 hover:scale-110 duration-300"
     >
       Home
     </button>
 
     <button
-      class="p-2 mr-2 rounded-md text-white hover:bg-slate-600 hover:scale-110 duration-300"
+      class="p-2 mr-10 ml-4 rounded-md text-white hover:bg-slate-800 hover:scale-110 duration-300"
     >
       Categories
     </button>
   </div>
-  <div class="bg-fixed">
+  <div class="bg-clip-text">
     <Image
       src="https://media.istockphoto.com/id/1289411982/vi/anh/an-ninh-m%E1%BA%A1ng-ph%C3%A1t-tri%E1%BB%83n-web-v%C3%A0-l%C3%A0m-vi%E1%BB%87c-trong-kh%C3%A1i-ni%E1%BB%87m-it.jpg?s=612x612&w=0&k=20&c=6NZTGJASgJN2vdY9sRVsxSg_UuNDo-qQpmen4hhafZ8="
     />
   </div>
 </template>
+
 <script setup></script>
