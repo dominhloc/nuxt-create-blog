9612430e7b84c8384139fdf824bab17836c594db
dominhloc <dominhloc2510@gmail.com>
2024-06-10 14:28:04 +0700

    update

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex justify-end mt-3 bg-slate-500 p-1 space-x-3">
+    <div class="flex-1">
+      <button
+        class="text-start p-2 ml-2 rounded-md text-white hover:bg-slate-800 hover:scale-110 duration-300"
+      >
+        Fresh
+      </button>
+    </div>
+
+    <button
+      class="p-2 rounded-md text-white hover:bg-slate-800 hover:scale-110 duration-300"
+    >
+      Home
+    </button>
+
+    <button
+      class="p-2 mr-2 rounded-md text-white hover:bg-slate-800 hover:scale-110 duration-300"
+    >
+      Categories
+    </button>
+  </div>
+</template>
+
+<script setup></script>
